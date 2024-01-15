4fd23efb5e3220f927a2f14a17b6d6676cdb1b8e
SamuMichea <sam.micheaa@gmail.com>
2024-01-15 03:00:24 -0300

    Tic Tac Toe

--- projects/tic-tac-toe/src/App.jsx
+++ projects/tic-tac-toe/src/App.jsx
@@ -1,35 +1,117 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+
+
+const Square = ({ children, isSelected, updateBoard, index}) => {
+  
+  const className = `square ${isSelected ? 'is-selected': ''}`
+
+  const handleClick= () => {
+    updateBoard(index)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onClick={handleClick}  className={className}>
+      {children}
+    </div>
   )
+}
+
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+function App() {
+  
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) 
+  // Null = no hay ganador, False hay empate
+
+  const checkWinner = (boardToCheck) => {
+    //revisamos todas las combinaciones posibles
+    for (const combo of WINNER_COMBOS){
+      const [a, b, c] = combo 
+      if (
+        boardToCheck[a] && 
+        boardToCheck[a] == boardToCheck[b] && 
+        boardToCheck[a] == boardToCheck[c] 
+        ) {
+          return boardToCheck[a]
+        }
+    }
+    // no hay ganador
+    return null
+  }
+
+  const updateBoard = (index) => {
+    // no actualizamos esta pos si ya tiene algo
+    if (board[index] || winner) return
+    // -------
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    // cambiamos el turno
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    // revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner){
+      setWinner((prevWinner)=>{
+        console.log(`El ganador es ${newWinner}, el anterior ganador era ${prevWinner}`)
+        return newWinner
+      })
+      // setWinner(newWinner) 
+      // Las actualizaciones de estados en React son Asincronos (Async)
+      // Por lo que al realizar tareas al mismo tiempo sin necesidad de que esta termine
+      // Causa que la alerta salte antes de que el estado del ganador sea actualizado
+      // Lo que da como resultado que al finalizar el juego, el estado siga siendo null
+      // alert(`El ganador es ${newWinner}`)
+    }
+  }
+
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <section className='game'>
+        {
+          board.map((_,index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn == TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn == TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
+  )    
 }
 
 export default App
