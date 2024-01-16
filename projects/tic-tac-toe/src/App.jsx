90dcd66c9ecafacb3aba70a9fdd478ef47d37a10
SamuMichea <sam.micheaa@gmail.com>
2024-01-16 17:43:54 -0300

    Tic Tac Toe - funcional

--- projects/tic-tac-toe/src/App.jsx
+++ projects/tic-tac-toe/src/App.jsx
@@ -1,38 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-
-
-const Square = ({ children, isSelected, updateBoard, index}) => {
-  
-  const className = `square ${isSelected ? 'is-selected': ''}`
-
-  const handleClick= () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick}  className={className}>
-      {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinnerFrom, checkEndGame } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
 
 function App() {
   
@@ -42,21 +14,15 @@ function App() {
   const [winner, setWinner] = useState(null) 
   // Null = no hay ganador, False hay empate
 
-  const checkWinner = (boardToCheck) => {
-    //revisamos todas las combinaciones posibles
-    for (const combo of WINNER_COMBOS){
-      const [a, b, c] = combo 
-      if (
-        boardToCheck[a] && 
-        boardToCheck[a] == boardToCheck[b] && 
-        boardToCheck[a] == boardToCheck[c] 
-        ) {
-          return boardToCheck[a]
-        }
-    }
-    // no hay ganador
-    return null
+  
+
+  const resetGame = ()=>{
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
   }
+
+  
 
   const updateBoard = (index) => {
     // no actualizamos esta pos si ya tiene algo
@@ -69,24 +35,28 @@ function App() {
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
     // revisar si hay ganador
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinnerFrom(newBoard)
+    
     if (newWinner){
-      setWinner((prevWinner)=>{
-        console.log(`El ganador es ${newWinner}, el anterior ganador era ${prevWinner}`)
-        return newWinner
-      })
-      // setWinner(newWinner) 
+      confetti()
+      setWinner(newWinner)
+      
+    } else if(checkEndGame(newBoard)){
+      setWinner(false)
+    }// check if game is over
+
+    // setWinner(newWinner) 
       // Las actualizaciones de estados en React son Asincronos (Async)
       // Por lo que al realizar tareas al mismo tiempo sin necesidad de que esta termine
       // Causa que la alerta salte antes de que el estado del ganador sea actualizado
       // Lo que da como resultado que al finalizar el juego, el estado siga siendo null
       // alert(`El ganador es ${newWinner}`)
-    }
   }
 
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Empezar de nuevo</button>
       <section className='game'>
         {
           board.map((_,index) => {
@@ -96,7 +66,7 @@ function App() {
                 index={index}
                 updateBoard={updateBoard}
               >
-                
+                {board[index]}
               </Square>
             )
           })
@@ -110,6 +80,9 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+      
+      <WinnerModal resetGame = {resetGame} winner = {winner}></WinnerModal>
+      
     </main>
   )    
 }
